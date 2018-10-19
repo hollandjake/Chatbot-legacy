53eb83d23a9652b86d2a8fdb0caa86f203e82094
Jake Holland <hollandjake@hotmail.co.uk>
2018-10-19 21:12:15 +0100

    Resolves #13
    Implemented !cat

--- src/main/java/bot/utils/Reddit.java
+++ src/main/java/bot/utils/Reddit.java
@@ -2,17 +2,25 @@ package bot.utils;
 
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static bot.utils.CONSTANTS.GET_RANDOM;
 
 public class Reddit {
-    public static String getSubredditPicture(List<String> subreddits) {
+    public static Image getSubredditPicture(List<String> subreddits) {
         while (subreddits != null) {
             //Pick subreddit
             String subreddit = GET_RANDOM(subreddits);
@@ -21,31 +29,43 @@ public class Reddit {
             String redditPath = "https://www.reddit.com/r/" + subreddit + "/random.json";
 
             try {
-                JSONArray data = (JSONArray) new JSONParser().parse(
-                        Unirest.get(redditPath)
+                String data = Unirest.get(redditPath)
                                 .header("User-agent", "Dogbot Reborn")
                                 .asString()
-                                .getBody()
-                );
-                String imageID = ((String)
-                        ((JSONObject)
-                                ((JSONObject)
-                                        ((JSONArray)
-                                                ((JSONObject)
-                                                        ((JSONObject) data.get(0))
-                                                                .get("data"))
-                                                        .get("children"))
-                                                .get(0))
-                                        .get("data"))
-                                .get("url"));
-
-                return imageID;
-            } catch (ParseException e) {
-                e.printStackTrace();
+                        .getBody();
+                Matcher matcher = Pattern.compile("https://i\\.redd\\.it/\\S+?\\.jpg").matcher(data);
+                if (matcher.find()) {
+                    BufferedImage image = ImageIO.read(new URL(matcher.group()));
+                    int size = image.getData().getDataBuffer().getSize();
+                    if (size < 25000000) {
+                        return image;
+                    }
+                }
             } catch (UnirestException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
         return null;
+    }
+
+    public static List<String> loadSubreddits(File subredditFile) {
+        try {
+            if (subredditFile.exists()) {
+                return new BufferedReader(new FileReader(subredditFile)).lines().collect(Collectors.toList());
+            } else {
+                File directory = subredditFile.getParentFile();
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+                subredditFile.createNewFile();
+                throw new IOException();
+            }
+        } catch (IOException e) {
+            System.out.println("No subreddits available for this session, maybe the file didn't exist");
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
     }
 }
